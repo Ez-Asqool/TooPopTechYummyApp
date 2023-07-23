d72cce5f3bb16bb73f6ac0f9630518193da01ce2
Ez-Asqool <ezasqool@gmail.com>
2023-07-23 12:21:52 +0300

    fgdfg

--- YummyApp.app/Controllers/HomeController.cs
+++ YummyApp.app/Controllers/HomeController.cs
@@ -5,7 +5,6 @@ using YummyApp.app.Models;
 
 namespace YummyApp.app.Controllers
 {
-    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
